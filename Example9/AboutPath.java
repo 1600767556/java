bc71942b92fab3001d9936fe2715ecd19358eb74
shaoshao <1600767556@qq.com>
2020-05-22 14:39:12 +0800

    测试提交

--- Example9/AboutPath.java
+++ Example9/AboutPath.java
@@ -7,7 +7,9 @@ import java.io.FileReader;
 public class AboutPath {
     public static void main(String[] args) throws Exception{
         //FileReader reader = new FileReader("F:\\ideaworkspace\\src\\Example9\\classinfo.properties");
-String path = Thread.currentThread().getContextClassLoader().getResource("classinfo2.properties").getPath();
+String path = Thread.currentThread().getContextClassLoader().getResource("Example9/classinfo2.properties").getPath();
         System.out.println(path);
+       String  path2 = Thread.currentThread().getContextClassLoader().getResource("Example9/bean/db.properties").getPath();
+        System.out.println(path2);
     }
 }

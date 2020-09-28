1556204c5d2b49c05f9f1c6a936440c0703c9086
shaoshao <1600767556@qq.com>
2020-09-28 11:13:52 +0800

    测试提交

---
+++ test/Test1.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2020/9/28 11:11
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
